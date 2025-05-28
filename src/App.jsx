7015e290fe52a3d12306d4833394ed955f233a03
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-05-28 13:42:03 +0100

    add new component on app.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
-import PostDetails from "./components/PostDetail";
+import PostDetail from "./components/PostDetail";
 import PostItem from "./components/PostItem";
 import PostsList from "./features/posts/PostsList";
+import CategoriesFilter from "./components/CategoriesFilter";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<PostsList />} />
-        <Route path="/post/:postId" element={<PostDetails />} />
+        <Route path="/post/:subreddit/:postId" element={<PostDetail />} />
       </Routes>
     </Router>
   );
