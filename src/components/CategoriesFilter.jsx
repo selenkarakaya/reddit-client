cd57e7db4014794c4ae7e933341e5ffa37ace3a0
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-05-28 16:01:39 +0100

    update category style

--- src/components/CategoriesFilter.jsx
+++ src/components/CategoriesFilter.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { fetchPosts } from "../features/posts/postsSlice";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPosts, setSelectedCategory } from "../features/posts/postsSlice";
 
 const categories = [
   "popular",
@@ -13,8 +13,11 @@ const categories = [
 
 function CategoriesFilter() {
   const dispatch = useDispatch();
+  const selectedCategory = useSelector((state) => state.posts.selectedCategory);
 
   const handleCategoryClick = (category) => {
+    if (category === selectedCategory) return; //Do not fetch if the same category is clicked
+    dispatch(setSelectedCategory(category));
     dispatch(fetchPosts(category));
   };
 
@@ -24,7 +27,12 @@ function CategoriesFilter() {
         <button
           key={category}
           onClick={() => handleCategoryClick(category)}
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
+          className={`px-4 py-2 rounded transition
+            ${
+              selectedCategory === category
+                ? "bg-blue-600 text-white"
+                : "bg-blue-300 text-gray-800 hover:bg-blue-400"
+            }`}
         >
           {category}
         </button>
