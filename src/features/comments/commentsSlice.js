7dec5f6526f6f90698661cd407eb1aa511bfe37d
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-05-28 13:41:08 +0100

    Create comments slice for fetch post's comments

---
+++ src/features/comments/commentsSlice.js
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchComments = createAsyncThunk(
+  "comments/fetchComments",
+  async (permalink) => {
+    const response = await fetch(`https://www.reddit.com${permalink}.json`);
+    if (!response.ok) throw new Error("Failed to fetch comments");
+    const data = await response.json();
+
+    return data[1].data.children.map((child) => child.data);
+  }
+);
+
+const commentsSlice = createSlice({
+  name: "comments",
+  initialState: {
+    items: [],
+    status: "idle",
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchComments.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchComments.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.items = action.payload;
+      })
+      .addCase(fetchComments.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default commentsSlice.reducer;
