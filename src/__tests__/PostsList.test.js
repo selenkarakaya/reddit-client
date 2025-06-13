82f1b76c7c1427750803e878d6042fbace66210d
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-06-13 17:03:37 +0100

    test PostsList component

--- src/__tests__/PostsList.test.js
+++ src/__tests__/PostsList.test.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import PostsList from "../features/posts/PostsList";
 import { MemoryRouter } from "react-router-dom";
 
