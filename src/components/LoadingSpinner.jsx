390e52432dc32ec657cadf828b33118c6feea431
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-06-13 15:25:54 +0100

    add [role] for loading spinner for test

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function LoadingSpinner() {
   return (
-    <div className="flex justify-center items-center h-32">
+    <div className="flex justify-center items-center h-32" role="status">
       <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-blue-500"></div>
     </div>
   );
